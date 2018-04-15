155fab6c877080ced1f5f5cded0e91ee185722d5
Iskander Broere <iskanderbroere@gmail.com>
2018-04-15 14:33:05 +0200

    works

--- pages/_id.vue
+++ pages/_id.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>{{ event }}</p>
+  <article>
+    <h1>{{ event.name }}</h1>
+    <ul>
+      <li v-for="comment in event.comments.items" :key="comment.commentId">
+        {{ comment.content }}
+      </li>
+    </ul>
+  </article>
 </template>
 
 <script>
@@ -9,52 +16,56 @@ import COMMENT_SUBSCRIBE from "~/apollo/subscriptions/commentSubscription.gql"
 export default {
   data() {
     return {
-      event: {}
+      event: {
+        comments: {
+          items: []
+        }
+      }
     }
   },
   apollo: {
     event: {
       query: EVENT_GET,
+      prefetch: true,
       subscribeToMore: {
-        document: COMMENT_SUBSCRIBE
-      },
-      updateQuery: (
-        prev,
-        {
-          subscriptionData: {
-            data: { subscribeToEventComments }
+        document: COMMENT_SUBSCRIBE,
+        variables() {
+          return { id: this.$route.params.id }
+        },
+        updateQuery: (
+          prev,
+          {
+            subscriptionData: {
+              data: { subscribeToEventComments }
+            }
           }
-        }
-      ) => {
-        debugger
-        const res = {
-          ...prev,
-          ...{
-            getEvent: {
-              ...prev.getEvent,
-              comments: {
-                __typename: "CommentConnections",
-                items: [
-                  ...prev.getEvent.comments.items.filter(c => {
-                    return (
-                      c.content !== subscribeToEventComments.content &&
-                      c.createdAt !== subscribeToEventComments.createdAt &&
-                      c.commentId !== subscribeToEventComments.commentId
-                    )
-                  }),
-                  subscribeToEventComments
-                ]
+        ) => {
+          const res = {
+            ...prev,
+            ...{
+              getEvent: {
+                ...prev.getEvent,
+                comments: {
+                  __typename: "CommentConnection",
+                  items: [
+                    ...prev.getEvent.comments.items.filter(c => {
+                      return c.commentId !== subscribeToEventComments.commentId
+                    }),
+                    subscribeToEventComments
+                  ]
+                }
               }
             }
           }
+          return res
         }
-
-        return res
       },
       variables() {
         return { id: this.$route.params.id }
       },
-      update: data => data.getEvent,
+      update: data => {
+        return data.getEvent
+      },
       error(error) {
         console.error("We've got an error!", error)
       }
